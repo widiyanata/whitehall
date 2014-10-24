3c0171cdb04f5b0ea58fd158511d18bb059ac850
Paul Hayes <fofr@users.noreply.github.com>
2014-10-24 13:17:37 +0100

    Don't use a list for organisations

--- app/helpers/admin/statistics_announcements_helper.rb
+++ app/helpers/admin/statistics_announcements_helper.rb
@@ -1,8 +1,7 @@
 module Admin::StatisticsAnnouncementsHelper
 
   def organisations_list(organisations)
-    content_tag(:ul,
-      raw(organisations.map { |organisation| content_tag(:li, organisation.name) }.join("\n")))
+    organisations.join(', ')
   end
 
 end
