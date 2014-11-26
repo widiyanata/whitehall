421d9b164ea6fb5fcb29e68a6838f841a813b7d7
Kips Davenport <patrick.davenport@unboxedconsulting.com>
2014-11-26 13:40:34 +0000

    WIP

--- lib/data_hygiene/tag_changes_processor.rb
+++ lib/data_hygiene/tag_changes_processor.rb
@@ -2,47 +2,72 @@ class TagChangesProcessor
 
   def initialize(csv_location)
     @csv_location = csv_location
-    @source_topic_id = ''
-    @destination_topic_id = ''
   end
 
   attr_reader :csv_location
-  attr_accessor :source_topic_id, :destination_topic_id
 
   def process
     CSV.foreach(csv_location, headers: true) do |taggings|
-      self.source_topic_id = taggings["add_topic"]
-      self.destination_topic_id = taggings["remove_topic"]
+      source_topic_id = taggings["add_topic"]
+      destination_topic_id = taggings["remove_topic"]
 
-      processor
+      processor(source_topic_id, destination_topic_id)
     end
   end
 
-  private
+private
 
-  def processor
+  def csv_to_array
+    csv = CSV::parse(File.open(csv_location.to_s, 'r') {|f| f.read })
+    fields = csv.shift
+    csv.collect { |record| Hash[*fields.zip(record).flatten ] }
+  end
+
+  def processor(source_topic_id, destination_topic_id)
     taggings, published_editions = get_taggings_and_editions(source_topic_id)
 
     log "Updating #{taggings.count} taggings of editions (#{published_editions.count} published) to change #{source_topic_id} to #{destination_topic_id}"
-    update_taggings(taggings)
+    update_taggings(taggings, source_topic_id, destination_topic_id)
     register_editions(published_editions)
   end
 
-  def update_taggings(taggings)
+  def update_taggings(taggings, source_topic_id, destination_topic_id)
     taggings.reject { |tagging| tagging.edition.nil? }.each do |tagging|
       if tagging.edition.specialist_sector_tags.include? destination_topic_id
-        remove_tagging(tagging)
+        remove_tagging(tagging, source_topic_id, destination_topic_id)
       else
-        change_tagging(tagging)
-      end
-    end
+        change_tagging(tagging, source_topic_id, destination_topic_id)
+      end                                                                             #TODO: where file?
+    end                                                                               #TODO: rake
   end
+
+  def remove_tagging(tagging, source_topic_id, destination_topic_id)
+    edition = tagging.edition
+    log "removing tagging on '#{edition.slug}' edition #{edition.id}"
+    tagging.destroy
+
+    add_editorial_remark(edition,
+      "Bulk retagging from topic '#{source_topic_id}' to '#{destination_topic_id}' resulted in duplicate tag - removed it"
+    )
+  end
+
+  def change_tagging(tagging, source_topic_id, destination_topic_id)
+    edition = tagging.edition
+    log "tagging '#{edition.slug}' edition #{edition.id}"
+    tagging.tag = destination_topic_id
+    tagging.save!
+
+    add_editorial_remark(edition,
+      "Bulk retagging from topic '#{source_topic_id}' to '#{destination_topic_id}' changed tag"
+    )
+  end
+
 
   def get_taggings_and_editions(topic_id)
     taggings = SpecialistSector.where(tag: topic_id)
 
     published_editions = taggings.map { |tagging|
-      tagging.edition.try(:document).try(:published_edition)
+      tagging.edition.latest_published_edition
     }.compact.uniq
 
     [taggings, published_editions]
@@ -57,6 +82,8 @@ class TagChangesProcessor
       register_with_search(edition)
     end
   end
+#the foloowing need to only action on the latest published for the slug...
+#latest_published_edition
 
   def register_with_panopticon(edition)
     registerable_edition = RegisterableEdition.new(edition)
@@ -65,33 +92,13 @@ class TagChangesProcessor
   end
 
   def register_with_publishing_api(edition)
-    PublishingApiWorker.perform_async(edition.class.name, edition.id)
+    PublishingApiWorker.perform_async(edition.class.name, edition.id, update_type: :republish)
   end
 
   def register_with_search(edition)
     ServiceListeners::SearchIndexer.new(edition).index!
   end
 
-  def remove_tagging(tagging)
-    edition = tagging.edition
-    log "removing tagging on '#{edition.slug}' edition #{edition.id}"
-    tagging.destroy
-
-    add_editorial_remark(edition,
-      "Bulk retagging from topic '#{source_topic_id}' to '#{destination_topic_id}' resulted in duplicate tag - removed it"
-    )
-  end
-
-  def change_tagging(tagging)
-    edition = tagging.edition
-    log "tagging '#{edition.slug}' edition #{edition.id}"
-    tagging.tag = destination_topic_id
-    tagging.save!
-
-    add_editorial_remark(edition,
-      "Bulk retagging from topic '#{source_topic_id}' to '#{destination_topic_id}' changed tag"
-    )
-  end
 
   def add_editorial_remark(edition, message)
     if edition.nil?
