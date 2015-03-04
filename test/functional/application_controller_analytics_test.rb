31a7200dce5eb20d542cc9b78d7be82b70b064ac
Paul Hayes <fofr@users.noreply.github.com>
2015-03-04 10:06:39 +0000

    Don't set a proposition header
    The header was used to set a proposition custom variable in Google
    Analytics. This variable is no longer used.
    
    * Stop setting the header
    * Remove tests

--- test/functional/application_controller_analytics_test.rb
+++ test/functional/application_controller_analytics_test.rb
@@ -2,8 +2,6 @@ require "test_helper"
 
 class ApplicationControllerAnalyticsTest < ActionController::TestCase
   class TestController < ApplicationController
-    def test_proposition; render text: "ok"; end
-
     class Organisation < Struct.new(:analytics_identifier); end
 
     def test_organisations
@@ -19,16 +17,6 @@ class ApplicationControllerAnalyticsTest < ActionController::TestCase
   end
 
   tests TestController
-
-  test "sets google analytics proposition to government for all actions" do
-    with_routing do |map|
-      map.draw do
-        get '/test_proposition', to: 'application_controller_analytics_test/test#test_proposition'
-      end
-      get :test_proposition
-    end
-    assert_equal "government", response.headers["X-Slimmer-Proposition"]
-  end
 
   test "sets google analytics organisation header to the passed in org list" do
 
