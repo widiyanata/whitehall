a9ac911eac1dfa8394c24f4406a221271f5fab21
Benjamin Welby <ben.welby@digital.cabinet-office.gov.uk>
2015-04-30 17:57:14 +0100

    HGW page no longer requires dynamic content
    The changes to the How Government Works page remove the count of policies, ministers and organisations.
    
    Reshuffle mode hid the ministerial module from the page, that module is no longer there

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,18 +7,6 @@ class HomeController < PublicFacingController
     @recently_updated = Edition.published.in_reverse_chronological_order.includes(:document).limit(10)
   end
 
-  def how_government_works
-    sitewide_setting = load_reshuffle_setting
-    @is_during_reshuffle = sitewide_setting.on if sitewide_setting
-    @policy_count = Policy.published.count
-    sorter = MinisterSorter.new
-    @cabinet_minister_count = sorter.cabinet_ministers.count - 1 # subtract one to discount PM
-    @other_minister_count = sorter.other_ministers.count
-    @all_ministers_count = @cabinet_minister_count + @other_minister_count + 1 # add one to put the PM back in
-    @ministerial_department_count = Organisation.listable.ministerial_departments.count
-    @non_ministerial_department_count = Organisation.listable.non_ministerial_departments.count
-  end
-
   def get_involved
     @open_consultation_count = Consultation.published.open.count
     @closed_consultation_count = Consultation.published.closed_since(1.year.ago).count
