bca18b901f41718d28549a778362f4358efe9eae
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2015-05-06 15:18:51 +0100

    Publish via the EditionForcePublisher.
    This makes more of the normal processes work whilst
    still not doing things like sending emails.

--- lib/tasks/election/policy_paper_publisher.rb
+++ lib/tasks/election/policy_paper_publisher.rb
@@ -11,15 +11,24 @@ module Election
             Rails.logger.warn("WARNING: Policy paper ##{policy_paper_id} already published")
           else
             Rails.logger.info("Force publishing policy paper ##{policy_paper_id} as a minor change")
-            policy_paper.minor_change = true
-            policy_paper.major_change_published_at = policy_paper.first_published_at
-            policy_paper.force_publish!
+            policy_paper.minor_change = false
+
+            Edition::AuditTrail.acting_as(gds_user) do
+              EditionForcePublisher.new(policy_paper).perform!
+            end
+
             policy_paper.update_column(:public_timestamp, 1.day.ago)
           end
         else
           Rails.logger.warn("WARNING: Couldn't find policy paper ##{policy_paper_id}")
         end
       end
+    end
+
+  private
+
+    def gds_user
+      @gds_user ||= User.find_by(email: "govuk-whitehall@digital.cabinet-office.gov.uk")
     end
   end
 end
