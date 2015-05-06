763e8b9b1eb6d069f645d1af6d49bd96fe51786d
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-05-06 16:35:04 +0100

    Removing FeatureFlagging from Policy feature
    As we no longer make use of the FeatureFlag for the policy fields, this
    commit removes it from the feature test and the support file which is no
    longer used.

--- features/support/future_policies.rb
+++
@@ -1,6 +0,0 @@
-# Turn on temporary feature-flag for future-policies feature during selected tests
-Around("@future-policies") do |scenario, block|
-  FeatureFlag.find_or_create_by(key: 'future_policies')
-  FeatureFlag.set('future_policies', true)
-  block.call
-end
